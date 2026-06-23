7a0c846b6f62c47bbb47b8dbe44d51c022b89020
Tauqeer-code-craft <tauqeerahmed90673@gmail.com>
2026-06-23 18:43:04 +0530

    react query added

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// Initialize Tanstack query client
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    }
+  }
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
